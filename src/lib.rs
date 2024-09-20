916c25f03af7b2dda36edabde9eff80fb9f70282
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-09-20 15:28:22 +0900

    :sparkles: add draw diamond and lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+fn draw_diamond(context: &web_sys::CanvasRenderingContext2d) {
+    context.set_line_width(5.0);
+    context.begin_path();
+    context.move_to(300.0, 50.0);
+    context.line_to(50.0, 300.0);
+    context.line_to(300.0, 550.0);
+    context.line_to(550.0, 300.0);
+    context.line_to(300.0, 50.0);
+    context.close_path();
+    context.stroke();
+}
 
-// This is like the `main` function, except for JavaScript.
+fn draw_middle_line(context: &web_sys::CanvasRenderingContext2d) {
+    context.set_line_width(5.0);
+    context.begin_path();
+    context.move_to(175.0, 175.0);
+    context.line_to(425.0, 425.0);
+    context.stroke();
+}
+
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
@@ -19,6 +31,24 @@ pub fn main_js() -> Result<(), JsValue> {
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
+
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    draw_diamond(&context);
+    draw_middle_line(&context);
 
     Ok(())
 }
